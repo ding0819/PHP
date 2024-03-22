6422a690b3234cedf184cf8df543a6d5fcad4abe
ding0819 <125901016+ding0819@users.noreply.github.com>
2024-03-22 17:17:27 +0800

    Create HW03.php

---
+++ week03/HW03.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <title>履歷表</title>
+</head>
+<body>
+    <font size=7 face="標楷體"><center>履歷表</center></font><br/>
+    <center><img src="../PHP/img/me.jpg" width="250" height="300"></center>
+    <center><h1>一、個人資料</h1></center>
+    </table>
+    <center><table border="1"></center>
+    <tr><th>姓名<th\><td>丁泳儷<td\><tr\>
+    <tr><th>出生日期<th\><td>2004/08/19<td\><tr\>
+    <tr><th>性別<th\><td>女<td\><tr\>
+    <tr><th>年齡<th\><td>19歲<td\><tr\>
+    <tr><th>通訊號碼<th\><td>0989095766<td\><tr\>
+    <tr><th>居住地<th\><td>高雄市<td\><tr\>
+    <tr><th>E-mail<th\><td>a1113353@mail.nuk.edu.tw<td\><tr\>
+    <tr><th>學歷<th\><td>大專院校<td\><tr\>
+    <tr><th>是否在學<th\><td>是<td\><tr\>
+    <tr><th>就讀學校<th\><td>高雄大學<td\><tr\>
+    <tr><th>系級<th\><td>資訊管理學系二年級<td\><tr\>
+    <tr><th>興趣<th\><td>打球、戶外運動、看電影<td\><tr\>
+    <tr><th>個性<th\><td>活潑、外向、熱情<td\><tr\>
+    </table><br/>
+    <center><h1>二、自我介紹</h1></center>
+    <font size="4">是個喜歡愛笑的人，都說愛笑的人運氣不會太差，我怎麼跟人家相反，再繼續加油吧!</font>
+
+</body>
+</html>

c31e70fcb68b9fc58aa7ac1aad23f0d181c5ff6e
ding0819 <125901016+ding0819@users.noreply.github.com>
2024-04-18 11:43:29 +0800

    Create showreview.php

---
+++ midexam/showreview.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+?>
+<html>
+<meta charset="utf-8">
+
+<?php
+    
+    $rOpinion=$_POST["rOpinion"];
+    echo "論文評審決定：".$rOpinion."<br/>";
+    if($_POST["rComment"]!="")
+    {
+        $rComment=$_POST["rComment"];
+        echo "論文評論提供：<br/>【".$rComment."】<br/>";
+    }
+    else
+    {
+        echo "[未提供論文評論!!!]";
+    }
+
+    if($_SESSION["check2"]=="Yes")
+    {
+        echo "<a href='logout.php'>登出</a>";
+    }
+    else
+    {
+        header("Location:error.php");
+    }
+?>
+</html>

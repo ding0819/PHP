809f891b8a7b19521c2554b31a6c5088b28f01b3
ding0819 <125901016+ding0819@users.noreply.github.com>
2024-04-18 11:41:13 +0800

    Update chair.php

--- midexam/chair.php
+++ midexam/chair.php
@@ -9,6 +9,7 @@
     if($_SESSION["check1"]=="Yes")
     {
         echo "歡迎進入網頁<br/>";
+        echo "帳號：chair" . "<br>" . "密碼：123" . "<br>" . "<br>";
         echo "<a href='logout.php'>登出</a>";
     }
     else

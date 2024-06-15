2964eaf42f8be7adf50d411db7c6aec7e522dbcd
ding0819 <125901016+ding0819@users.noreply.github.com>
2024-06-15 18:18:55 +0800

    Create mail.php

---
+++ HW06/mail.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+
+<form action="sendmail.php" method="post">
+    請輸入收件人信箱：<input type="text" name="to"><br/>
+    請輸入信箱標題：<input type="text" name="subject"><br/>
+    請輸入信件內容：<textarea rows="10" cols="30" name="content"></textarea><br/>
+    <input type="submit"><br/>
+</form>

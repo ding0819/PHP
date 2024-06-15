76e44a725ba9d89781b50f86fa3d340908f924bd
ding0819 <125901016+ding0819@users.noreply.github.com>
2024-06-15 18:10:54 +0800

    Create login.php

---
+++ HW05/login.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <center><title>使用者登入系統</title></center>
+</head>
+    <h1><center>使用者登入系統</center></h1>
+<form action="HW05check.php" method="post">
+    <h3><center>帳號：<input type="text" name="uID" value="" placeholder="請輸入帳號"></center></h3>
+    <h3><center>密碼：<input type="password" name="uPWD" value="" placeholder="請輸入密碼"></center></h3>
+    <h3><center><input type="submit" value="Log in"></center></h3>
+</form>
+</html>

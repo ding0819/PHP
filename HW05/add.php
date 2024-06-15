fe747b8696290ea821f31e82be53ef4f9b0f9b8e
ding0819 <125901016+ding0819@users.noreply.github.com>
2024-06-15 18:04:32 +0800

    Create add.php

---
+++ HW05/add.php
@@ -0,0 +1,11 @@
+<meta  charest='utf8'>
+
+<form action="adddb.php" method="get">
+
+    學號：<input type="text" name="sNo"><br/>
+    姓名：<input type="text" name="sName"><br/>
+    系所：<input type="text" name="sDept"><br/>
+    電話：<input type="email" name="s"><br/>
+    <input type="submit" value="提交">
+    
+</form>

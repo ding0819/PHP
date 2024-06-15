d8c6cb89c851d6fe6325d65e5caf7bdca8982587
ding0819 <125901016+ding0819@users.noreply.github.com>
2024-06-15 18:54:31 +0800

    Create index.php

---
+++ HW06/index.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+
+<form action="sentmail.php" method="post">
+    請輸入郵件標題:<input type="text" name="subject"><br><br>
+    請輸入郵件內容:<textarea rows="10" cols="20" name="content"></textarea><br>
+    <input type="submit" value="送出">
+</form>
+
+<a href='add.php'>新增資料</a><br>
+<a href='show.php'>查看資料庫內容</a>

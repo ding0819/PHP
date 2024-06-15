f7ce9b662412f42ffb874cdc8cb2a9e6ac3c06d0
ding0819 <125901016+ding0819@users.noreply.github.com>
2024-06-15 18:09:14 +0800

    Create uploadcheck.php

---
+++ HW05/uploadcheck.php
@@ -0,0 +1,23 @@
+<meta charset='utf8'>
+
+<?php
+    echo "檔案名稱: " . $_FILES["myFile"]["name"] . "<br>";
+    echo "檔案檔名: " . $_FILES["myFile"]["tmp_name"] . "<br>";
+    echo "檔案尺寸: " . $_FILES["myFile"]["size"] . "<br>";
+    echo "檔案種類: " . $_FILES["myFile"]["type"] . "<br>";
+
+    echo $ext = pathinfo($_FILES["myFile"]["name"], PATHINFO_EXTENSION);
+
+    $upname = time(). "." . $ext;
+
+    $upname = "pic\\".$upname;
+    echo $upname;
+
+    if(copy($_FILES["myFile"]["tmp_name"], $upname)) {
+        echo "檔案上傳成功";
+        unlink($_FILES["myFile"]["tmp_name"]);
+    }
+
+    echo "<br><a href='upload.php'>回上一頁</a><br>";
+    echo "<a href='login.php'>回首頁</a>";
+?>

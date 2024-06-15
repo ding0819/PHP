1bd4214b1cf2b5df8831203f589210e4d6d7813e
ding0819 <125901016+ding0819@users.noreply.github.com>
2024-06-15 16:51:11 +0800

    Create fail.php

---
+++ HW04/fail.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<?php
+    session_start();
+    header("Refresh:3 url=HW05login.php");
+?>
+<html>
+<head>
+</head>
+<body>
+    <h1><center>登入系統失敗</center></h1>
+    <h1><center>帳號/密碼有誤</center></h1>
+    <h1><center>3秒後重回登入頁面</center></h1>
+    <center><img src=../PHP/img/貓咪.jpg width="750" height="600"></center>
+</body>
+</html>
